286bb2d1cc466cbc51c0e0eb43e48d44345e7036
Celia <celia.bruner12@gmail.com>
2023-09-14 13:24:11 -0400

    first commit

---
+++ 8kyu/multiply.sql
@@ -0,0 +1,10 @@
+-- Multiply --
+DESCRIPTION:
+This code does not execute properly. Try to figure out why.
+
+SELECT price + amount AS total FROM items
+
+-- Solution --
+SELECT price * amount
+AS total
+FROM items;
